955d98e61cdf4cd566da132851f2d7b7a666c116
Diksha Jadhav <jadhavdiksha91@gmail.com>
2025-01-02 18:34:36 +0530

    First commit

---
+++ src/com/demo/ArrayListDemo.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+
+		   
+		    ArrayList<String> languages = new ArrayList<>();
+
+		    
+		    languages.add("Java");
+		    languages.add("Python");
+		    languages.add("Swift");
+		    System.out.println("ArrayList: " + languages);
+		  }
+		
+	}
+
+
